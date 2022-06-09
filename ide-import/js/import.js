82dd5c89d1fadab805b5c0a9ea0afecd204be64f
StanZGenchev <stanislav.genchev@sap.com>
2022-06-09 21:16:49 +0300

    Import fixes, import dialog added

--- ide-import/js/import.js
+++ ide-import/js/import.js
@@ -13,37 +13,78 @@ let importView = angular.module('import', ['ideUI', 'ideView', 'ideWorkspace', '
 
 importView.controller('ImportViewController', [
     '$scope',
+    '$window',
     'messageHub',
     'workspaceApi',
     'transportApi',
     'FileUploader',
     function (
         $scope,
+        $window,
         messageHub,
         workspaceApi,
         transportApi,
         FileUploader,
     ) {
-        let transportUrl = transportApi.getProjectImportUrl();
+        let projectImportUrl = transportApi.getProjectImportUrl();
         $scope.selectedWorkspace = { name: 'workspace' }; // Default
         $scope.workspaceNames = [];
+        $scope.inDialog = false;
+        $scope.inputAccept = '.zip';
+        $scope.dropAreaTitle = 'Import Projects';
+        $scope.dropAreaSubtitle = 'Drop zip file(s) here, or use the "+" button.';
+        $scope.dropAreaMore = '';
+        $scope.uploadPath = '';
         $scope.uploader = new FileUploader({
-            url: transportUrl
+            url: projectImportUrl
         });
+
+        $scope.getViewParameters = function () {
+            let params = JSON.parse($window.frameElement.getAttribute("data-parameters"));
+            if (params.container === 'dialog') {
+                $scope.inDialog = true;
+                $scope.uploadPath = params.uploadPath;
+                $scope.selectedWorkspace.name = params.workspace;
+                if (params.importType === 'file') {
+                    $scope.inputAccept = '';
+                    $scope.importType = params.importType;
+                    $scope.dropAreaTitle = 'Import files';
+                    $scope.dropAreaSubtitle = 'Drop file(s) here, or use the "+" button.';
+                    $scope.dropAreaMore = `Files will be imported in "${params.uploadPath}"`;
+                } else {
+                    $scope.dropAreaTitle = 'Import files from zip';
+                    $scope.dropAreaMore = `Files will be extracted in "${params.uploadPath}"`;
+                    let pathSegments = params.uploadPath.split('/');
+                    $scope.uploader.url = new UriBuilder().path(transportApi.getZipImportUrl().split('/')).path(params.workspace).path(pathSegments).build();
+                    if (pathSegments.length <= 2) $scope.uploader.url += '/%252F';
+                }
+            }
+            else $scope.reloadWorkspaceList();
+        }
 
         $scope.uploader.filters.push({
             name: 'customFilter',
             fn: function (item /*{File|FileLikeObject}*/, options) {
                 let type = item.type.slice(item.type.lastIndexOf('/') + 1);
-                if (type != 'zip' && type != 'x-zip' && type != 'x-zip-compressed') {
-                    return false;
-                }
+                if ($scope.importType !== 'file')
+                    if (type != 'zip' && type != 'x-zip' && type != 'x-zip-compressed') {
+                        return false;
+                    }
                 return this.queue.length < 100;
             }
         });
 
         $scope.uploader.onBeforeUploadItem = function (item) {
-            item.url = transportUrl + "/" + $scope.selectedWorkspace.name;
+            if (!$scope.inDialog) {
+                item.url = new UriBuilder().path(projectImportUrl.split('/')).path($scope.selectedWorkspace.name).build();
+            } else if ($scope.inDialog && $scope.importType === 'file') {
+                item.headers = {
+                    'Dirigible-Editor': 'Editor',
+                    'Content-Type': 'application/octet-stream',
+                    'Content-Transfer-Encoding': 'base64'
+                };
+                item.url = new UriBuilder().path('/services/v4/ide/workspaces'.split('/')).path($scope.selectedWorkspace.name).path(uploadPath.split('/')).path(item.name).build();
+            }
         };
 
         $scope.uploader.onCompleteAll = function () {
@@ -61,7 +102,7 @@ importView.controller('ImportViewController', [
         };
 
         $scope.addFiles = function () {
-            document.getElementById('input').click();
+            $window.document.getElementById('input').click();
         };
 
         $scope.switchWorkspace = function (workspace) {
@@ -93,5 +134,5 @@ importView.controller('ImportViewController', [
         );
 
         // Initialization
-        $scope.reloadWorkspaceList();
+        $scope.getViewParameters();
     }]);
